ed31d8def0012ba383ee46f26d7b7bd7d19900f3
PORTJUSTO1@GMAIL.COM <portjusto1@gmail.com>
2023-04-13 16:34:18 +0100

    LPP

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,50 +1,44 @@
 #!/usr/bin/python3
-"""
-log parsing
-"""
+""" A script that reads stdin line by line and computes metrics """
 
 import sys
-import re
 
 
-def output(log: dict) -> None:
-    """
-    helper function to display stats
-    """
-    print("File size: {}".format(log["file_size"]))
-    for code in sorted(log["code_frequency"]):
-        if log["code_frequency"][code]:
-            print("{}: {}".format(code, log["code_frequency"][code]))
+def print_status(dict, size):
+    """Print the format"""
+    print("File size: {}".format(size))
+    for key in sorted(dict.keys()):
+        if dict[key] != 0:
+            print("{}: {}".format(key, dict[key]))
 
 
-if __name__ == "__main__":
-    regex = re.compile(
-    r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - \[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d+\] "GET /projects/260 HTTP/1.1" (.{3}) (\d+)')  # nopep8
+status_dict = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0,
+               '404': 0, '405': 0, '500': 0}
 
-    line_count = 0
-    log = {}
-    log["file_size"] = 0
-    log["code_frequency"] = {
-        str(code): 0 for code in [
-            200, 301, 400, 401, 403, 404, 405, 500]}
+file_size = 0
+count = 0
 
-    try:
-        for line in sys.stdin:
-            line = line.strip()
-            match = regex.fullmatch(line)
-            if (match):
-                line_count += 1
-                code = match.group(1)
-                file_size = int(match.group(2))
+try:
+    for line in sys.stdin:
+        if count != 0 and count % 10 == 0:
+            print_status(status_dict, file_size)
 
-                # File size
-                log["file_size"] += file_size
+        el = line.split(" ")
+        count += 1
 
-                # status code
-                if (code.isdecimal()):
-                    log["code_frequency"][code] += 1
+        try:
+            file_size += int(el[-1])
+        except:
+            pass
 
-                if (line_count % 10 == 0):
-                    output(log)
-    finally:
-        output(log)
+        try:
+            if el[-2] in status_dict.keys():
+                status_dict[el[-2]] += 1
+        except:
+            pass
+    print_status(status_dict, file_size)
+
+
+except KeyboardInterrupt:
+    print_status(status_dict, file_size)
+    raise
