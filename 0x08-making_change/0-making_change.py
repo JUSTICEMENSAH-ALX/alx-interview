43b44b1cdd6686189f128e329354e903bd53cb8e
PORTJUSTO1@GMAIL.COM <portjusto1@gmail.com>
2023-05-18 11:33:23 +0100

    YYYY'

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,38 +1,23 @@
 #!/usr/bin/python3
 """
-Defines function that determines the fewest number of coins to make change
+Determine the fewestnumber of coins needed to
+meet a given amount total.
 """
 
 
 def makeChange(coins, total):
-    """
-    Determines the fewest number of coins needed to meet a given total
-
-    parameters:
-        coins [list or positive ints]:
-            the values of the coins in your possession
-            you can assume you have an infinite number of coins of all values
-        total [int]:
-            total amount of change to make
-            if total is 0 or less, return 0
-
-    returns:
-        the fewest number of coins to make the change
-        or -1 if the total change cannot be made with the given coins
-    """
-    if total <= 0:
+    """ makeChange module """
+    if total < 1:
         return 0
-    if len(coins) is 0:
+    coins.sort()
+    coins.reverse()
+    change = 0
+    for coin in coins:
+        if total <= 0:
+            break
+        temp = total // coin
+        change += temp
+        total -= (temp * coin)
+    if total != 0:
         return -1
-    coins = sorted(coins)
-    dynamic = [float('inf')] * (total + 1)
-    dynamic[0] = 0
-    for i in range(total + 1):
-        for coin in coins:
-            if coin > i:
-                break
-            if dynamic[i - coin] != -1:
-                dynamic[i] = min(dynamic[i - coin] + 1, dynamic[i])
-    if dynamic[total] == float('inf'):
-        return -1
-    return dynamic[total]
+    return change
